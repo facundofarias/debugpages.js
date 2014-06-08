8ded0ecf60f6f78631017b7e5f19050a1498ffcc
Facundo Farias <facundo.farias@gmail.com>
2014-06-08 20:28:06 +0200

    First version that seems to be working as expected

--- debugpages.js
+++ debugpages.js
@@ -1,10 +1,8 @@
-(function(document) {
-
-  "use strict";
+window.onload = function() {
 
   var span = document.createElement("span");
-  span.setAttribute("style", "background: #FA0303; color: #fff; text-align: center; font-weight: bold; font-size: 1rem; position: relative; transition: 0.5s; position: absolute; display: block; overflow: hidden; width: 200px; padding: 5px 40px; top: 60px; right: -60px; transform: rotate(45deg); -webkit-transform: rotate(45deg); box-shadow: 4px 4px 10px rgba(0,0,0,0.8);");
-  span.innerHTML = "You're browsing in <strong>DEBUG</strong> mode. Intended for quick read-only checks. Many features are disabled.";
+  span.setAttribute("style", "background: #FA0303; color: #fff; text-align: center; transition: 0.5s; position: absolute; display: block; overflow: hidden; width: 200px; padding: 5px 40px; top: 60px; right: -60px; transform: rotate(45deg); -webkit-transform: rotate(45deg); box-shadow: 4px 4px 10px rgba(0,0,0,0.8);");
+  span.innerHTML = "You're browsing in <strong>DEBUG</strong> mode.";
   document.body.appendChild(span);
 
   var links = document.links;
@@ -12,10 +10,17 @@
   {
     if (links[i])
     {
-      links[i].removeAttribute("href");
-      if(links[i] && links[i].hasAttribute("target"))
-        links[i].removeAttribute("target");
+      links[i].onclick = function() {
+		      return false;
+      };
     }
+  }
+
+  var inputs = document.getElementsByTagName("INPUT");
+  for (var i = 0; i < inputs.length; i++) {
+      if (inputs[i].type === 'submit') {
+          inputs[i].disabled = true;
+      }
   }
 
   var scripts = document.scripts;
@@ -24,4 +29,4 @@
     scripts[i].remove();
   }
 
-}(window.document));
+};
